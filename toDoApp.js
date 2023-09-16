550bb8477473f9e7ee1cafdc2b8c7caa46c73054
nguyenhungne <nguyenhungf205@gmail.com>
2023-09-16 10:27:53 +0700

    TEST

--- toDoApp.js
+++ toDoApp.js
@@ -12,7 +12,8 @@ function toDoApp () {
 
     function cancelHandel() { 
        const toDoHandle = document.querySelector('.to-do-handle');
-       toDoHandle.classList.add('hidden');
+       taskInput.value = '';
+       taskInput.focus();
     }
 
     function clearTasks () {
@@ -96,133 +97,129 @@ function toDoApp () {
 
     let tasksArray = [];
 
-let undoneTask = [];
-let doneTask=[];
-renderTask(tasksArray);
+    let undoneTask = [];
+    let doneTask=[];
 
-tasksArray.forEach((task) => {
-    if (task.done === false) {
-        undoneTask.push(task);
-    } else if(task.done === true) {
-        doneTask.push(task);
-    }
-})
 
-renderTask(undoneTask);
+    tasksArray.forEach((task) => {
+        if (task.done === false) {
+            undoneTask.push(task);
+        } else if(task.done === true) {
+            doneTask.push(task);
+        }
+    })
 
-//handle add task
-addButton.addEventListener('click', addTask);
-
-function addTask () {
-    
-  if (taskInput.value != '' | taskInput.value.trim()) { 
-        undoneTask.push({
-        task: taskInput.value,
-        done: false,
-    });
-    
-    taskInput.value = '';
-    taskInput.focus();
     renderTask(undoneTask);
-    select();
-    }
-}
 
-function select() {
+    //handle add task
+    addButton.addEventListener('click', addTask);
 
+    function addTask () {
         
+    if (taskInput.value != '' | taskInput.value.trim()) { 
+            undoneTask.push({
+            task: taskInput.value,
+            done: false,
+        });
+        
+        taskInput.value = '';
+        taskInput.focus();
+        renderTask(undoneTask);
+        filterInput.value='UNDONE';
+        select();
+        }
+    }
 
-    const editButton = document.querySelectorAll('.edit-button');
+    function select() {
+
+            
+
+        const editButton = document.querySelectorAll('.edit-button');
 
 
-    //handle edit task 
-    for (let i = 0; i < editButton.length; i++) {
-        editButton[i].addEventListener('click',editHandle);
+        //handle edit task 
+        for (let i = 0; i < editButton.length; i++) {
+            editButton[i].addEventListener('click',editHandle);
 
-        function editHandle () {
-                taskInput.value = '';
-                taskInput.focus();
-                undoneTask.splice(i, 1);
-                renderTask(undoneTask);
-                select();
+            function editHandle () {
+                    taskInput.value = '';
+                    taskInput.focus();
+                    undoneTask.splice(i, 1);
+                    renderTask(undoneTask);
+                    select();
+            }
+        }
+
+
+
+
+        //handle delete task:
+    const deleteButton = document.querySelectorAll('.delete-button');
+
+    for(let i = 0; i < deleteButton.length; i++) {
+            deleteButton[i].addEventListener('click', deleteHandle);
+
+            function deleteHandle() {
+                    undoneTask.splice(i, 1);
+                    renderTask(undoneTask);
+                    select();
+            }
+    }
+
+    const checkBoxDone = document.querySelectorAll('.to-do-done');
+
+    for (let i = 0; i < undoneTask.length; i++) {
+        console.log(checkBoxDone[i]);
+        checkBoxDone[i].addEventListener('change', checkedHandle);
+        function checkedHandle () {
+            undoneTask[i].done = true;
+            doneTask.push(undoneTask[i]);
+            undoneTask.splice(i, 1);
+            renderTask(undoneTask);
+            select();
+        }
+    }
+
+
+    }
+
+
+    const filterInput = document.getElementById('filter-options');
+    filterInput.addEventListener('change',filterInputHandler)
+
+    function filterInputHandler() {
+        if (filterInput.value === 'DONE') {
+            renderTaskDone(doneTask);
+            select();
+            
+    } else if (filterInput.value === 'UNDONE') {
+            renderTask(undoneTask);
+            select();
+    }
+
+    }
+
+
+
+    let currentAccount = JSON.parse(localStorage.getItem('loggedInAccount'));
+
+    console.log(currentAccount);
+
+    // person["address"] = "123 Main Street";
+
+    currentAccount["tasks"] = JSON.stringify(tasksArray);
+
+    localStorage.setItem('loggedInAccount', JSON.stringify(currentAccount));
+    };
+
+    toDoApp()
+
+    const checkLogIn = () => {
+        let loggedInAccount = JSON.parse(localStorage.getItem('loggedInAccount'));
+
+        if ((loggedInAccount === null)){
+            redirect();
+        }
+    }
 
     
-        }
-    }
-
-
-
-
-    //handle delete task:
-const deleteButton = document.querySelectorAll('.delete-button');
-
-for(let i = 0; i < deleteButton.length; i++) {
-        deleteButton[i].addEventListener('click', deleteHandle);
-
-        function deleteHandle() {
-                undoneTask.splice(i, 1);
-                renderTask(undoneTask);
-                select();
-        }
-}
-
-const checkBoxDone = document.querySelectorAll('.to-do-done');
-
-for (let i = 0; i < undoneTask.length; i++) {
-    console.log(checkBoxDone[i]);
-    checkBoxDone[i].addEventListener('change', checkedHandle);
-    function checkedHandle () {
-        console.log(undoneTask[i]);
-        undoneTask[i].done = true;
-        doneTask.push(undoneTask[i]);
-        undoneTask.splice(i, 1);
-        renderTask(undoneTask);
-        select();
-    }
- }
-
-
-}
-
-
-const filterInput = document.getElementById('filter-options');
-filterInput.addEventListener('change',filterInputHandler)
-
-function filterInputHandler() {
-    clearTasks();
-
-    if (filterInput.value === 'DONE') {
-        renderTaskDone(doneTask);
-        select();
-        
-} else if (filterInput.value === 'UNDONE') {
-        renderTask(undoneTask);
-        select();
-}
-
-}
-
-
-
-let currentAccount = JSON.parse(localStorage.getItem('loggedInAccount'));
-
-console.log(currentAccount);
-
-// person["address"] = "123 Main Street";
-
-currentAccount["tasks"] = JSON.stringify(tasksArray);
-
-localStorage.setItem('loggedInAccount', JSON.stringify(currentAccount));
-};
-
-toDoApp()
-
-const checkLogIn = () => {
-    let loggedInAccount = JSON.parse(localStorage.getItem('loggedInAccount'));
-
-    if ((loggedInAccount === null)){
-        redirect();
-    }
- }
-
- 
